f9dc8743c673d14a425fe820256927754fcac7d5
RifatShahriarRatul <rifat.sh.ratul@gmail.com>
2026-01-06 22:48:01 +0600

    Table complete

--- grade-sheet.c
+++ grade-sheet.c
@@ -10,22 +10,23 @@ int main()
     {
         scanf("%d", &mark[i]);
     }
+    printf("----------------------------\n");
     printf("Serial no.    Mark     Grade\n");
     printf("----------------------------\n");
     for(i=0; i<num; i++)
     {
-             if(mark[i]>79) printf("%d.             %d        A+", i+1, mark[i]);
-        else if(mark[i]>74) printf("%d.             %d        A", i+1, mark[i]);
-        else if(mark[i]>69) printf("%d.             %d        A-", i+1, mark[i]);
-        else if(mark[i]>64) printf("%d.             %d        B+", i+1, mark[i]);
-        else if(mark[i]>59) printf("%d.             %d        B", i+1, mark[i]);
-        else if(mark[i]>54) printf("%d.             %d        B-", i+1, mark[i]);
-        else if(mark[i]>49) printf("%d.             %d        C+", i+1, mark[i]);
-        else if(mark[i]>44) printf("%d.             %d        C", i+1, mark[i]);
-        else if(mark[i]>39) printf("%d.             %d        D", i+1, mark[i]);
-        else printf("%d.             %d        F", i+1, mark[i]);
+             if(mark[i]>79) printf("    %d.         %d        A+", i+1, mark[i]);
+        else if(mark[i]>74) printf("    %d.         %d        A", i+1, mark[i]);
+        else if(mark[i]>69) printf("    %d.         %d        A-", i+1, mark[i]);
+        else if(mark[i]>64) printf("    %d.         %d        B+", i+1, mark[i]);
+        else if(mark[i]>59) printf("    %d.         %d        B", i+1, mark[i]);
+        else if(mark[i]>54) printf("    %d.         %d        B-", i+1, mark[i]);
+        else if(mark[i]>49) printf("    %d.         %d        C+", i+1, mark[i]);
+        else if(mark[i]>44) printf("    %d.         %d        C", i+1, mark[i]);
+        else if(mark[i]>39) printf("    %d.         %d        D", i+1, mark[i]);
+        else printf("    %d.         %d        F", i+1, mark[i]);
         printf("\n");
-
     }
+    printf("----------------------------\n");
 
 }
