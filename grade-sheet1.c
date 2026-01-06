3d7c8bf10f59a1a437a78cae92a5b4ca3bf99c16
RifatShahriarRatul <rifat.sh.ratul@gmail.com>
2026-01-06 23:20:47 +0600

    Prject completed

--- grade-sheet1.c
+++ grade-sheet1.c
@@ -4,8 +4,10 @@ int main()
     int num, i, m,n,o,p, total_mark;
     printf("The number of students under consideration: ");
     scanf("%d", &num);
+    int mark[num];
     for(i=0; i<num; i++)
     {
+        printf("For student no. %d\n", i+1);
         printf("Enter your marks in Attendance: ");
         scanf("%d", &m);
         printf("Enter your marks in Term Test: ");
@@ -17,23 +19,27 @@ int main()
         if(m<=10 && n<=20 && o<=10 && p<=60)
         {
             total_mark = m+n+o+p;
-            printf("----------------------------\n");
-            printf("Serial no.    Mark     Grade\n");
-            printf("----------------------------\n");
-            if(total_mark>79) printf("    %d.         %d        A+", i+1, total_mark);
-        else if(total_mark>74) printf("    %d.         %d        A", i+1, total_mark);
-        else if(total_mark>69) printf("    %d.         %d        A-", i+1, total_mark);
-        else if(total_mark>64) printf("    %d.         %d        B+", i+1, total_mark);
-        else if(total_mark>59) printf("    %d.         %d        B", i+1, total_mark);
-        else if(total_mark>54) printf("    %d.         %d        B-", i+1, total_mark);
-        else if(total_mark>49) printf("    %d.         %d        C+", i+1, total_mark);
-        else if(total_mark>44) printf("    %d.         %d        C", i+1, total_mark);
-        else if(total_mark>39) printf("    %d.         %d        D", i+1, total_mark);
-        else printf("    %d.         %d        F", i+1, total_mark);
-        printf("\n");
-            printf("----------------------------\n");
+            mark[i] = total_mark;
         }else{
             printf("Your input is accurate");
         }
     }
+    printf("----------------------------\n");
+    printf("Serial no.    Mark     Grade\n");
+    printf("----------------------------\n");
+    for(i=0; i<num; i++)
+    {
+             if(mark[i]>79) printf("    %d.         %d        A+", i+1, mark[i]);
+        else if(mark[i]>74) printf("    %d.         %d        A", i+1, mark[i]);
+        else if(mark[i]>69) printf("    %d.         %d        A-", i+1, mark[i]);
+        else if(mark[i]>64) printf("    %d.         %d        B+", i+1, mark[i]);
+        else if(mark[i]>59) printf("    %d.         %d        B", i+1, mark[i]);
+        else if(mark[i]>54) printf("    %d.         %d        B-", i+1, mark[i]);
+        else if(mark[i]>49) printf("    %d.         %d        C+", i+1, mark[i]);
+        else if(mark[i]>44) printf("    %d.         %d        C", i+1, mark[i]);
+        else if(mark[i]>39) printf("    %d.         %d        D", i+1, mark[i]);
+        else printf("    %d.         %d        F", i+1, mark[i]);
+        printf("\n");
+    }
+    printf("----------------------------\n");
 }
